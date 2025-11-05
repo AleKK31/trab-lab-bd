1e59095708877d8d686acaec4b74101b0e0dfce9
Pablo Sena <pssena@vipech.com.br>
2025-11-04 22:45:29 -0300

    feat: adicionar configuração do banco de dados PostgreSQL no arquivo data-source.ts e criar compose.yaml

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,7 +11,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  url: process.env.DATABASE_URL,
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   synchronize: true,
   logging: true,
   entities: [
